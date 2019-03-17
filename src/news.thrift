b68c46e8a3ea0dd246d34a8d93261068b7b6af4b
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-03-17 12:10:59 +0700

    feat(news): create thrift definition for news

---
+++ src/news.thrift
@@ -0,0 +1,49 @@
+/*
+ * news.thrift
+ *
+ * @author Irfan Andriansyah <irfanandriansyah10@gmail.com>
+ *
+ * This file contains thrift definitions for UrbanIndo Criteria Property.
+ */
+
+namespace java com.efekRevisi.thrift.news
+namespace js efekRevisi.ThriftServices.news
+
+/**
+ * Struct for property criteria parameter.
+ */
+struct News {
+
+    1: required i64 newsID,
+
+    2: required string title,
+
+    3: required string thumbnail,
+
+    4: required string title,
+
+    5: required string description
+}
+
+struct NewsList {
+
+    1: required list<News> items;
+}
+
+/**
+ * Structs can also be exceptions, if they are nasty.
+ */
+exception ErrorHandling {
+  1: i32 idError,
+  2: string message
+}
+
+
+/**
+ * All of the endpoints are sessionfull.
+ */
+service NewsService {
+    NewsList getNews() throws (
+        1:ErrorHandling ex1
+    );
+}
